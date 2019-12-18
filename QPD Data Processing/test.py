417c0f87151177b8c5e7db293ac62e8e162ea9a1
Phil Earp <philip.earp@ukaea.uk>
2019-12-18 11:05:51 +0000

    Fundamental frequency plotting

--- QPD Data Processing/test.py
+++ QPD Data Processing/test.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import scipy.fftpack
 
+##
+f_osc = 20e3 # approx. oscillation frequency [Hz]
+f_samp = 100e3 # sampling frequency [Hz]
+
+times_to_display = [0.2, 0.2005]
+##
+
 raw = np.loadtxt('real test 2_19-11-28_15-36-41 (5).csv', dtype=float , delimiter=',', skiprows=2)
 t = raw[:,0] # time [microseconds]
 t = t - t[0] # elapsed time [microseconds]
@@ -10,6 +17,14 @@ t = t * 1e-6 # elapsed time [seconds]
 x = raw[:,1]
 y = raw[:,2]
 intensity = raw[:,3]
+
+ii = t < 0.6
+t = t[ii]
+x = x[ii]
+y = y[ii]
+intensity = intensity[ii]
+
+
 
 plt.plot(t, x, 'k-')
 plt.xlabel('time (s)', fontsize=15)
@@ -20,19 +35,19 @@ plt.figure()
 plt.plot(t*1000, x, 'ko-')
 plt.xlabel('time (ms)', fontsize=15)
 plt.ylabel('QPD x-value (V)', fontsize=15)
-plt.xlim(200, 200.5)
+plt.xlim(times_to_display[0] * 1000, times_to_display[1] * 1000)
 # plt.savefig('QPD Raw Data ON.png')
 
-f_osc = 20e3 # oscillation frequency [Hz]
 T_osc = 1 / f_osc # oscillation time period [s]
 
 plt.figure()
 plt.plot(t / T_osc, x, 'ko-')
 plt.xlabel('cycles (n)', fontsize=15)
 plt.ylabel('QPD x-value (V)', fontsize=15)
-plt.xlim(0.2 / T_osc, 0.2005 / T_osc)
+plt.xlim(times_to_display[0] / T_osc, times_to_display[1] / T_osc)
 
-f_samp = 100e3 # sampling frequency [Hz]
+## FFT calculation
+
 T_samp = 1 / f_samp # sampling time period [s]
 
 # calculate fft of QPD x signal
@@ -54,6 +69,19 @@ plt.plot(fftfreq[i], x_psd[i])
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('PSD (arb. units)')
 
+## Select fundamental frequency 
+
+x_fft_fund = x_fft.copy()
+x_fft_fund[np.abs(fftfreq > 20180)] = 0
+x_fft_fund[np.abs(fftfreq < 20172)] = 0
+
+x_fund = np.real(sp.fftpack.ifft(x_fft_fund))
+
+plt.figure()
+plt.plot(t, x, 'k-')
+plt.plot(t, x_fund + np.mean(x), 'r-')
+plt.xlabel('time (s)', fontsize=15)
+plt.ylabel('QPD x-value (V)', fontsize=15)
 
 plt.show()
 print('done')
