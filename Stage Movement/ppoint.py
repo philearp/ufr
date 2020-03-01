e8f88e24ad6cb490cfed27e4e3a7083f265246ee
Phil Earp <phil.earp@hotmail.com>
2020-03-01 14:48:59 +0000

    Added main function, added printing commands

--- Stage Movement/ppoint.py
+++ Stage Movement/ppoint.py
@@ -4,20 +4,25 @@ import matplotlib.pyplot as plt
 # Function Definitions
 
 def c(theta):
+    # theta [radians]
     return np.cos(theta)
 
 def s(theta):
+    # theta [radians]
     return np.sin(theta)
 
 def rotation_x(theta):
+    # theta [radians]
     R = np.array([[1, 0, 0], [0, c(theta), -s(theta)], [0, s(theta), c(theta)]])
     return R
 
 def rotation_y(theta):
+    # theta [radians]
     R = np.array([[c(theta), 0, s(theta)], [0, 1, 0], [-s(theta), 0, c(theta)]])
     return R
 
 def rotation_z(theta):
+    # theta [radians]
     R = np.array([[c(theta), -s(theta), 0], [s(theta), c(theta), 0], [0, 0, 1]])
     return R
 
@@ -32,34 +37,48 @@ def calc_rotation_matrix(theta_x_deg, theta_y_deg, theta_z_deg):
     R = np.matmul(R_x, R)
     return R
 
-# main code starts here:
+def main_ppoint_correction(CA_ref, QP, thetas_deg):
+    # inputs:
+    #   CA_ref = offset from stage rotation centre (needle tip) (C) to focal point of laser (A_ref) [mm]
+    #   QP = Vector from reference position on sample (Q) to desired position on sample (P) [mm]
+    #   thetas_deg = User tilt angles [degrees]
 
-# Define offset from stage rotation centre (needle tip) (C) to focal point of laser (A_ref)
-CA_ref = np.array([80, 0, 250]) # [mm]
+    # unpack thetas
+    theta_x_deg = thetas_deg[0]
+    theta_y_deg = thetas_deg[1]
+    theta_z_deg = thetas_deg[2]
 
-# Define desired user displacements
-ux = 0 # [mm]
-uy = 0 # [mm]
-uz = 0 # [mm]
+    # 3x3 Rotation matrix due to angles:
+    #  theta_x_deg about x-axis
+    #  theta_y_deg about y-axis
+    #  theta_z_deg about z-axis
+    R = calc_rotation_matrix(theta_x_deg, theta_y_deg, theta_z_deg)
+
+    # Calculate required stage displacement to position laser focus point at desired position on sample
+    OC = QP + CA_ref - np.matmul(R, CA_ref)
+
+    return OC
+
+# CODE BEGINS HERE
+
+# User inputs:
+# Define offset from stage rotation centre (needle tip) to focal point of laser
+optics_offset = np.array([80, 0, 240]) # [mm]
+
+# Define desired user displacements from reference position on sample
+user_translation = np.array([0, 0, 0]) # [mm]
 
 # Define desired user rotation angles
-theta_x_deg = 1 # [degrees]
-theta_y_deg = 0 # [degrees]
-theta_z_deg = 0 # [degrees]
+user_tilt = np.array([1, 0, 0]) #[degrees]
 
-# Vector from reference position on sample (Q) to desired position on sample (P)
-QP = np.array([ux, uy, uz]) # [mm]
+# Calculation of required stage translation:
+print("P-Point translation of:")
+print("({0:.2f}, {1:.2f}, {2:.2f}) mm.".format(user_translation[0], user_translation[1], user_translation[2]))
+print("({0:.2f}, {1:.2f}, {2:.2f}) degrees,".format(user_tilt[0], user_tilt[1], user_tilt[2]))
+print("for an optical set-up P-Point offset of ({0:.0f}, {1:.0f}, {2:.0f}) mm".format(optics_offset[0], optics_offset[1], optics_offset[2]))
 
-# 3x3 Rotation matrix due to angles:
-#  theta_x_deg about x-axis
-#  theta_y_deg about y-axis
-#  theta_z_deg about z-axis
-R = calc_rotation_matrix(theta_x_deg, theta_y_deg, theta_z_deg)
+required_stage_translation = main_ppoint_correction(optics_offset, user_translation, user_tilt)
 
-# Calculate required stage displacement to position laser focus point at desired position on sample
-OC = QP + CA_ref - np.matmul(R, CA_ref)
+print("r5equires stage translation of x = {0:.3f}mm, y = {1:.3f}mm, z = {2:.3f}mm".format(required_stage_translation[0], required_stage_translation[1], required_stage_translation[2]))
 
-print("Requires stage translation of:")
-print("x = {0:.3f}mm".format(OC[0]))
-print("y = {0:.3f}mm".format(OC[1]))
-print("z = {0:.3f}mm".format(OC[2]))
+
