8722dbe495ffabcdaeb0926cf43ee7d9ab9a9cc2
Phil Earp <philip.earp@ukaea.uk>
2020-04-08 14:03:55 +0100

    added logger

--- qpd-calibration/src/qpd-calibration.py
+++ qpd-calibration/src/qpd-calibration.py
@@ -1,4 +1,38 @@
 # %%
+def define_logger():
+    '''
+    The defined levels, in order of increasing severity, are the following:
+
+        DEBUG <-- least severe
+        INFO
+        WARNING
+        ERROR
+        CRITICAL <-- most severe
+    '''
+
+    # Create a custom logger
+    logger = logging.getLogger(__name__)
+
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('file.log')
+    c_handler.setLevel(logging.WARNING)
+    f_handler.setLevel(logging.ERROR)
+
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    logger.info('Logging Started')
+
+    return logger
+
 def load_calibration_data(filepath):
     """Loads UFR tilt calibration data from file.
 
@@ -481,6 +515,8 @@ def fit_qpd2theta_surface(grid_qx, grid_qy, tx_array, ty_array):
     return d
 
 def main_calibration():
+
+    logger.info(f'Start calibration using file: {filename}')
     
     calibration_data, calibration_data_angular_range = load_calibration_data(filename)
     if verbose_output:
@@ -555,10 +591,13 @@ from scipy import interpolate
 from scipy.optimize import fsolve
 import plotly.graph_objects as go
 import plotly.express as px
+import logging
 
 plot_figures = False
 verbose_output = False
 filename = '../data/2020-03-10_QPD-Tilt-Calibration_Test1.csv'
+
+logger = define_logger()
 
 d = main_calibration()
 
